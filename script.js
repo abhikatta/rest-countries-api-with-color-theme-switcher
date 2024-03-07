09f6204fb2816a007d429ca1b6964cc6ea6baf84
unknown <abhinaykatta97@gmail.com>
2024-03-07 20:04:11 +0530

    addded grid layout

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-const main = document.getElementById("main");
-const itemContainer = document.getElementById("item-container");
-// not using this as per design requirements
+const itemContainerElement = document.getElementById("item-container");
+const searchElement = document.getElementById("search");
+// not using this due to design requirements
 const URL = "https://restcountries.com/v3.1/all";
 const LOCAL_JSON_DATA_FILE = "./data.json";
 var req = new XMLHttpRequest();
 var countriesData = JSON.parse(localStorage.getItem("countriesData")) || [];
+var mapCountries = countriesData;
 window.addEventListener("load", async () => {
-  // AJAX: params:request type, url/file, async or not
-  //   req.open("GET", URL, true);
-  //   req.onload = function () {
-  //     if (this.status === 200) {
-  //       //   console.log(this.responseText);
-  //       countriesData = JSON.parse(this.responseText);
-  //       console.log(countriesData);
-  //       countriesData = JSON.stringify(countriesData);
-  //       main.textContent = countriesData;
-  //     }
-  //   };
-  //   req.send();
-  //   Good Ol' Fetch:
   fetch(LOCAL_JSON_DATA_FILE)
     .then((response) => response.json())
     .then((data) => {
@@ -31,15 +19,10 @@ window.addEventListener("load", async () => {
       } else {
         console.log("fetched data from localStorage");
       }
-      //   data.forEach((element) => {
-      //     main.innerHTML += `<p>${element.name.official}</p>`;
-      //     main.innerHTML += `<img width='100' alt=${element.flags.alt} src=${element.flags.svg}></img>`;
-      //   });
     })
     .catch((error) => console.log(error));
-  countriesData.map((v, i) => {
-    console.log(v);
-    itemContainer.innerHTML += `<div class='item' id='item'>
+  mapCountries.map((v, i) => {
+    itemContainerElement.innerHTML += `<div class='item' key=${i} id='item'>
                                 <img  src=${v.flags.png} alt=${v.flags.alt}/>
                                 <p class='country-title'>${v.name.common}</p>
                                 <p class='country-population'>Population: ${v.population}</p>
@@ -48,13 +31,18 @@ window.addEventListener("load", async () => {
   });
 });
 
-// console.log(countriesData.length);
-// countriesData.map((v, i) => {
-//   console.log(v);
-//   itemContainer.innerHTML += `<div class='item' id='item'>
-//                                 <img  src=${v.flags.png} alt=${v.flags.alt}/>
-//                                 <p class='country-title'>${v.name.common}</p>
-//                                 <p class='country-population'>Population: ${v.population}</p>
-//                                 <p class='country-capital'>Capital: ${v.capital}</p>
-//                             </div>`;
-// });
+const search = () => {
+  if (searchElement.value.length > 0) {
+    console.log(search.value);
+    mapCountries
+      .filter((v) => v.includes(search.value))
+      .map((v, i) => {
+        itemContainer.innerHTML += `<div class='item' id='item'>
+                                            <img class='country-flag' src=${v.flags.png} alt=${v.flags.alt}/>
+                                            <p class='country-title'>${v.name.common}</p>
+                                            <p class='country-population'>Population: ${v.population}</p>
+                                            <p class='country-capital'>Capital: ${v.capital}</p>
+                                        </div>`;
+      });
+  }
+};
