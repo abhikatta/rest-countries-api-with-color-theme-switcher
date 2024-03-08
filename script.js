8658f50b2b0d9c67706e1de44d482e9b1d89807b
unknown <abhinaykatta97@gmail.com>
2024-03-08 13:07:59 +0530

    improved rendering performance by using DOM API instead of inserting innerHTML

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ const darkModeButton = document.getElementById("darkmode-button");
 const body = document.body;
 const item = document.getElementById("item");
 
-// links/data or related:
-// not using this due to design requirements
-const URL = "https://restcountries.com/v3.1/all";
-const LOCAL_JSON_DATA_FILE = "./data.json";
-var countriesData = JSON.parse(localStorage.getItem("countriesData")) || [];
-var mapCountries = countriesData;
 // var req = new XMLHttpRequest();
-// event listeners:
-window.addEventListener("load", async () => {
+
+// as of now, not using this due to design requirements and changes in
+// latest api format of rest-countries:
+// const URL = "https://restcountries.com/v3.1/all";
+
+const LOCAL_JSON_DATA_FILE = "./data.json";
+let countriesData = JSON.parse(localStorage.getItem("countriesData")) || [];
+let mapCountries = countriesData;
+const renderCountryDetails = () => {
   fetch(LOCAL_JSON_DATA_FILE)
     .then((response) => response.json())
     .then((data) => {
@@ -28,34 +29,49 @@ window.addEventListener("load", async () => {
         console.log("fetched data from localStorage");
       }
     })
-    .catch((error) => console.log(error));
-  mapCountries.map((v, i) => {
-    itemContainerElement.innerHTML += `<div class='item' key=${i} id='item'>
-                                <img  src=${v.flags.png} alt=${v.flags.alt}/>
-                                <p class='country-title'>${v.name.common}</p>
-                                <p class='country-population'>Population: ${v.population}</p>
-                                <p class='country-capital'>Capital: ${v.capital}</p>
-                            </div>`;
-  });
-});
-// dark mode event listener:
+    .catch((error) => {
+      body.innerHTML += "<h1>Something Went Wrong</h1>";
+      console.log(error);
+    });
+  let countryComponents = mapCountries.map((v, i) => {
+    let itemDiv = document.createElement("div");
+    itemDiv.className = "item";
+    itemDiv.id = "item";
 
-const search = () => {
-  if (searchElement.value.length > 0) {
-    console.log(search.value);
-    mapCountries
-      .filter((v) => v.includes(search.value))
-      .map((v, i) => {
-        itemContainer.innerHTML += `<div class='item' id='item'>
-                                            <img class='country-flag' src=${v.flags.png} alt=${v.flags.alt}/>
-                                            <p class='country-title'>${v.name.common}</p>
-                                            <p class='country-population'>Population: ${v.population}</p>
-                                            <p class='country-capital'>Capital: ${v.capital}</p>
-                                        </div>`;
-      });
-  }
+    let flag = document.createElement("img");
+    flag.src = v.flags.png;
+    flag.alt = v.flags.alt;
+
+    let title = document.createElement("p");
+    title.className = "country-title";
+    let titleText = document.createTextNode(v.name);
+    title.appendChild(titleText);
+
+    let population = document.createElement("p");
+    population.className = "country-population";
+    let populationText = document.createTextNode(v.population);
+    population.appendChild(populationText);
+
+    let capital = document.createElement("p");
+    capital.className = "country-capital";
+    let capitalText = document.createTextNode(v.capital);
+    capital.appendChild(capitalText);
+
+    itemDiv.append(flag, title, population, capital);
+    return itemDiv;
+
+    // itemContainerElement.innerHTML += ` <div class='item' key=${i} id='item'>
+    //                                       <img  src=${v.flags.png} alt=${v.flags.alt}/>
+    //                                       <p class='country-title'>${v.name}</p>
+    //                                       <p class='country-population'>Population: ${v.population}</p>
+    //                                       <p class='country-capital'>Capital: ${v.capital}</p>
+    //                                     </div>`;
+  });
+  countryComponents.map((v) => itemContainerElement.append(v));
 };
+// event listeners:
+window.addEventListener("load", renderCountryDetails);
+
 darkModeButton.addEventListener("click", () => {
   console.log("dark mode button pressed");
-  body.classList.toggle("darkMode");
 });
